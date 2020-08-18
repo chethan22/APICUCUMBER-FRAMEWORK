855947585e00f4621783105e4828871aa8affad3
CHETHAN <CHETHAN@hp>
2020-08-18 22:56:17 +0530

    updated

--- src/test/java/StepDefination/get_Operation.java
+++ src/test/java/StepDefination/get_Operation.java
@@ -34,7 +34,6 @@ public class get_Operation {
 		Response a4 = a3.then().extract().response();
 		System.out.println(a4.asString());
 		System.out.println(a4.getStatusLine());
-		System.out.println(a4.getStatusLine());
 		Assert.assertEquals(a, a4.getStatusCode());
 
 	}
